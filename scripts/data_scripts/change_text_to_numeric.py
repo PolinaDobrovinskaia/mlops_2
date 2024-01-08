003b0c114ee528948adb2e6b462b4415df08b644
PolinaDobrovinskaia <114981740+PolinaDobrovinskaia@users.noreply.github.com>
2024-01-08 15:12:48 +0100

    Create change_text_to_numeric.py

---
+++ scripts/data_scripts/change_text_to_numeric.py
@@ -0,0 +1,22 @@
+cimport sys
+import os
+import pandas as pd
+
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython3 change_text_to_numeric.py data-file\n")
+    sys.exit(1)
+
+f_input = sys.argv[1]
+os.makedirs(os.path.join("data", "stage3"), exist_ok=True)
+
+# забираем датасет для обработки
+df = pd.read_csv(f_input)
+
+# Удалим категориальные данные
+df.drop(['Make',	'Model',	'Style',	'Fuel_type',	'Transmission'],
+axis=1, inplace=True)
+
+
+df.to_csv("data/stage3/train.csv", index=False)
