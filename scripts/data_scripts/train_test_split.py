7abc8e2ef56c9c4e2a4233327f30d54b307320eb
PolinaDobrovinskaia <114981740+PolinaDobrovinskaia@users.noreply.github.com>
2024-01-08 15:16:36 +0100

    Create train_test_split.py

---
+++ scripts/data_scripts/train_test_split.py
@@ -0,0 +1,27 @@
+import yaml
+import sys
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython3 train_test_split.py data-file\n")
+    sys.exit(1)
+
+f_input = sys.argv[1]
+
+os.makedirs(os.path.join("data", "stage4"), exist_ok=True)
+
+# забираем датасет для обработки
+df = pd.read_csv(f_input)
+
+X = df.drop(columns = ["Price(euro)"])
+y = df["Price(euro)"]
+features_names = df.drop(columns = ["Price(euro)"]).columns
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+pd.concat([X_train, y_train], axis=1).to_csv("data/stage4/train.csv", index=None)
+pd.concat([X_test, y_test], axis=1).to_csv("data/stage4/test.csv", index=None)
